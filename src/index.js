71d74fed403e837a0ea53a7839c3132a821e5f05
Yuri Kovalev <yurec.0gurec99@gmail.com>
2018-09-22 16:57:49 +0300

    passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var leng = bracketsConfig.length;
+  var pair = str.length * 0.5;
+  for (var i = 0; i < pair; i++) {
+      for (var b = 0; b < str.length; b++) {
+          for (var k = 0; k < leng; k++) {
+              if (str[b] === bracketsConfig[k][0] && str[b + 1] === bracketsConfig[k][1]) {
+                  var helpArr = [];
+                  for (var j = 0; j < str.length; j++) {
+                      if(j === b || j === b + 1) continue;
+                     helpArr.push(str[j]);
+                  }
+                  str = helpArr;
+              }
+          }
+      }
+  }
+
+  if (str.length === 0) return true;
+
+  else return false;
 }
+
